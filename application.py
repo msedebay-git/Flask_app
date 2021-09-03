a9b4e6805d0d6f22e3cabf9c1a7018eb2530c2f3
Talent Network <mohammedsedebay@gmail.com>
2021-09-02 21:31:58 -0400

    modified application.py

--- application.py
+++ application.py
@@ -3,4 +3,4 @@ application = Flask(__name__)
 
 @application.route('/')
 def hello_world():
-     return '<h1><center>Welcome to my Flask App!</center></h1>'
+     return '<h1><center>Welcom to Flask application!</center></h1>'
